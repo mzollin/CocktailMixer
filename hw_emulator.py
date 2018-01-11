230654286edb9d9b2fb6ad7849696568a0f17872
Marco Zollinger <marco@freelabs.space>
2018-01-11 01:45:35 +0100

    add license header

--- hw_emulator.py
+++ hw_emulator.py
@@ -1,3 +1,20 @@
+# Copyright 2017 Marco Zollinger <marco@freelabs.space>
+#
+# This file is part of CocktailMixer.
+#
+# CocktailMixer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CocktailMixer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CocktailMixer.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 from PyQt5.QtCore import QIODevice, pyqtSignal, Qt
 from PyQt5.QtWidgets import QApplication, QStyleFactory, QWidget, QGridLayout, QSizePolicy, QPushButton, QLabel, QScrollBar
